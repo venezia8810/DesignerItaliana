01929bc5d6f6ff46bf6555dee3963c55b987774c
venezia8810 <lovetwister8810@gmail.com>
2019-10-08 00:16:59 +0900

    second vommit

---
+++ design/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="utf-8">
+  <title>Quiz</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<section>
+	<h1>
+		italiano
+	</h1>
+	<p>
+		devo imparare da solo 
+	</p>
+
+</section>
+
+
+
